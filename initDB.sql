e9a7a1f0f16b9b53f606b79d8af30ebf1c2bf71e
fortamt <90449535+fortamt@users.noreply.github.com>
2022-02-08 14:29:16 +0200

    Update initDB.sql

--- initDB.sql
+++ initDB.sql
@@ -1,6 +1,6 @@
 CREATE TABLE  developers
 (
-	developer_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	developer_name VARCHAR(30) NOT NULL,
 	developer_age INT,
 	developer_sex VARCHAR(1) NOT NULL
@@ -8,28 +8,28 @@ CREATE TABLE  developers
 
 CREATE TABLE skills
 (
-	skill_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	language VARCHAR(15) NOT NULL,
 	skill_lvl VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE projects
 (
-	project_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	project_name VARCHAR(30) NOT NULL,
 	project_start_date DATE
 );
 
 CREATE TABLE companies
 (
-	company_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	company_name VARCHAR(30) NOT NULL,
 	company_address VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE customers
 (
-	customer_id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	customer_name VARCHAR(30) NOT NULL,
 	customer_phone VARCHAR(20) NOT NULL
 );
@@ -38,8 +38,8 @@ CREATE TABLE Developers_Projects_Relation
 (
 	developer_id INT NOT NULL,
 	project_id INT NOT NULL,
-	FOREIGN KEY (developer_id) REFERENCES developers (developer_id),
-	FOREIGN KEY (project_id) REFERENCES projects (project_id),
+	FOREIGN KEY (developer_id) REFERENCES developers (id),
+	FOREIGN KEY (project_id) REFERENCES projects (id),
 	UNIQUE (developer_id, project_id)
 );
 
@@ -47,8 +47,8 @@ CREATE TABLE Developers_Skills_Relation
 (
 	developer_id INT NOT NULL,
 	skill_id INT NOT NULL,
-	FOREIGN KEY (developer_id) REFERENCES developers (developer_id),
-	FOREIGN KEY (skill_id) REFERENCES skills (skill_id),
+	FOREIGN KEY (developer_id) REFERENCES developers (id),
+	FOREIGN KEY (skill_id) REFERENCES skills (id),
 	UNIQUE (developer_id, skill_id)
 );
 
@@ -56,8 +56,8 @@ CREATE TABLE Companies_Projects_Relation
 (
 	company_id INT NOT NULL,
 	project_id INT NOT NULL,
-	FOREIGN KEY (company_id) REFERENCES companies (company_id),
-	FOREIGN KEY (project_id) REFERENCES projects (project_id),
+	FOREIGN KEY (company_id) REFERENCES companies (id),
+	FOREIGN KEY (project_id) REFERENCES projects (id),
 	UNIQUE (company_id, project_id)
 );
 
@@ -65,8 +65,8 @@ CREATE TABLE Customers_Projects_Relation
 (
 	customer_id INT NOT NULL,
 	project_id INT NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
-	FOREIGN KEY (project_id) REFERENCES projects (project_id),
+	FOREIGN KEY (customer_id) REFERENCES customers (id),
+	FOREIGN KEY (project_id) REFERENCES projects (id),
 	UNIQUE (customer_id, project_id)
 );
 
